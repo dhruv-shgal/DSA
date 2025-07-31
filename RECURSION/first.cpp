343a80b79d3da93a09dc6671abf533afc3510fac
Dhruv Sehgal <dhruvsehgal.022@gmail.com>
2025-07-31 20:02:25 +0530

    Initial commit

---
+++ RECURSION/first.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void fun(int n) {
+    if (n == 0) return;
+    cout << n << " ";
+    fun(n - 1);
+}
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    fun(n);
+    return 0;
+}
