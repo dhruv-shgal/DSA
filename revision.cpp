2f6e568e3617c760b58405bc137b9ac1cabe8b0e
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-08-25 13:08:00 +0530

    Initial commit

--- revision.cpp
+++ revision.cpp
@@ -1,11 +1,20 @@
 #include<bits/stdc++.h>
 int main(){
     int arr[5]={2,5,1,3,0};
-    int largest=arr[0];
-    for(int i=0;i<5;i++){
-        if(arr[i]>largest){
-            largest=arr[i];
+    // int largest=arr[0];
+    // for(int i=0;i<5;i++){
+    //     if(arr[i]>largest){
+    //         largest=arr[i];
+    //     }
+    // }
+    // std::cout<<"the largest number is "<<largest;
+    for(int i=1;i<5;i++){
+        if(arr[i]>=arr[i-1]){
+            continue;
         }
+        else{
+            std::cout<<"not sorted";
+        }
+        
     }
-    std::cout<<"the largest number is "<<largest;
 }
